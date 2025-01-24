a12d9cdf06496d9e9058a2c504361df16e89ccad
johneyder <john.kazz@gmail.com>
2025-01-24 12:08:11 -0500

    test with fetch manual

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,13 +1,23 @@
 import { fetchQuotations } from "@/lib/data";
 import type { Route } from "./+types/test";
+import { BASE_URL } from "@/lib/constants";
 
 export async function loader() {
-  const quotations = await fetchQuotations();
-  return { quotations };
+  try {
+    const res = await fetch(`${BASE_URL}/api/quotations`);
+    if (!res.ok) throw new Error("Error fetching quotations in server");
+    const data = await res.json();
+    return {
+      quotations: data.items,
+    };
+  } catch (error) {
+    throw new Response("Erorr server", { status: 500 });
+  }
 }
 
 export default function testPage({ loaderData }: Route.ComponentProps) {
   const { quotations } = loaderData;
+  console.log(quotations);
   return (
     <div>
       <pre>{JSON.stringify(quotations, null, 2)}</pre>
