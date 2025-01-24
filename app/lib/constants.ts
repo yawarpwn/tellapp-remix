c108f6221516f818ffb75cbe003126c435bcc881
yawarpwn <john.kazz@gmail.com>
2025-01-23 23:38:06 -0500

    update to production

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = "http://localhost:8787";
+export const BASE_URL = "https://hono-tell-api.tellsenales.workers.dev";
--- app/lib/data/index.ts
+++ app/lib/data/index.ts
@@ -1,10 +1,10 @@
-const URL = "http://localhost:8787";
+import { BASE_URL } from "../constants";
 import type { QuotationClient } from "../types";
 
 export async function fetchQuotations(): Promise<QuotationClient[]> {
   // await new Promise((resolve) => setTimeout(resolve, 5000));
   // console.log("resolved");
-  const res = await fetch(`${URL}/api/quotations`);
+  const res = await fetch(`${BASE_URL}/api/quotations`);
   if (!res.ok) throw new Error("Failed to fetch quotations");
   const data = (await res.json()) as { items: QuotationClient[] };
   return data.items;
@@ -12,9 +12,9 @@ export async function fetchQuotations(): Promise<QuotationClient[]> {
 
 export async function fetchQuotaitonByNumber(quotationNumber: number) {
   console.log(quotationNumber);
-  const url = `${URL}/api/quotations/${quotationNumber}`;
+  const url = `${BASE_URL}/api/quotations/${quotationNumber}`;
   console.log(url);
-  const res = await fetch(`${URL}/api/quotations/${quotationNumber}`);
+  const res = await fetch(`${BASE_URL}/api/quotations/${quotationNumber}`);
   if (!res.ok) throw new Error("Failed to fetch quotations");
   const data = (await res.json()) as QuotationClient;
   return data;
