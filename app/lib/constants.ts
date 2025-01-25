23b739d3882fd7eedf450221f4453f562f2dd4c5
yawarpwn <john.kazz@gmail.com>
2025-01-24 23:14:14 -0500

    try fix

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,1 +1,2 @@
+// export const BASE_URL = "http://localhost:8787";
 export const BASE_URL = "https://api.tellsenales.workers.dev";
--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -6,16 +6,20 @@ export async function loader() {
   try {
     const res = await fetch(`${BASE_URL}/api/quotations`);
     if (!res.ok) throw new Error("Failed to fetch quotations");
+
     const data = await res.json();
-    return data.items;
+    console.log(data.items);
+    return {
+      quotations: data.items,
+    };
   } catch (error) {
-    throw new Response("Erorr server", { status: 500 });
+    throw new Response("Erorr server", { status: 404 });
   }
 }
 
 export default function testPage({ loaderData }: Route.ComponentProps) {
   const { quotations } = loaderData;
-  console.log(quotations);
+  console.log("quos", quotations);
   return (
     <div>
       <pre>{JSON.stringify(quotations, null, 2)}</pre>
