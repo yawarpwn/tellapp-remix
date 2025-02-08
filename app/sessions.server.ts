64cc8f5abecb2696c1af3856bb40409309948efe
yawarpwn <john.kazz@gmail.com>
2025-02-07 22:16:21 -0500

    remove domain

--- app/sessions.server.ts
+++ app/sessions.server.ts
@@ -16,7 +16,7 @@ const { getSession, commitSession, destroySession } =
       name: '__session',
 
       // all of these are optional
-      domain: import.meta.env.PROD ? PRODUCTION_URL : undefined,
+      // domain: import.meta.env.PROD ? PRODUCTION_URL : undefined,
       // Expires can also be set (although maxAge overrides it when used in combination).
       // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
       //
