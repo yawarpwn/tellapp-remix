3ba4c9df7ab6662af9b357d77b03faed25b4e388
yawarpwn <john.kazz@gmail.com>
2025-02-21 22:46:03 -0500

    fix search customer by ruc

--- app/routes/quotations/search-by-ruc.tsx
+++ app/routes/quotations/search-by-ruc.tsx
@@ -1,15 +1,15 @@
 import { searchCustomerByDniOrRuc } from '@/lib/data'
-import { data } from 'react-router'
 import type { Route } from './+types/search-by-ruc'
 import { handleError } from '@/lib/utils'
-import { getTokenFromSession } from '@/sessions.server'
 
-export async function action({ request }: Route.ActionArgs) {
+export async function action({ request, context }: Route.ActionArgs) {
   try {
-    const token = await getTokenFromSession(request)
     const formData = await request.formData()
     const ruc = formData.get('ruc') as string
-    const customer = await searchCustomerByDniOrRuc(ruc, token)
+    const customer = await searchCustomerByDniOrRuc(
+      ruc,
+      context.cloudflare.env.TELL_API_KEY
+    )
     return {
       success: true,
       customer,
