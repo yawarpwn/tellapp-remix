aaab7997b0c6f3805e3ba6c272d2c66830d42bc6
yawarpwn <john.kazz@gmail.com>
2025-03-03 01:48:53 -0500

    update to 40

--- app/lib/data/index.ts
+++ app/lib/data/index.ts
@@ -26,7 +26,7 @@ export async function fetchQuotations(
   { query, page }: { query: string; page: number; limit: number }
 ): Promise<QuotationClient[]> {
   console.log('fetch quotations')
-  const url = `${BASE_URL}/api/quotations?q=${query}&page=${page}&limit=100`
+  const url = `${BASE_URL}/api/quotations?q=${query}&page=${page}&limit=40`
   const data = await fetchData<DataResponse<QuotationClient>>(url, {
     headers: {
       'TELL-API-KEY': apiKey,
